8444617e469735086bb21c23f2c796b242f271e9
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-12-17 22:47:43 +0100

    TIC-TAC-TOE graph

--- script.py
+++ script.py
@@ -1,5 +1,13 @@
 player1_name = input('Player\'s name 1: ')
 player2_name = input('Player\'s name 2: ')
 
-print('Player 1 is {p1_name}, player 2 is {p2_name}'.format(
+print('Hello {p1_name} and {p2_name}, welcome to'.format(
     p1_name=player1_name, p2_name=player2_name))
+
+print('''
+####### #  ####     #######  #    ####     #######  ####  ####
+   #       #           #    # #   #           #    #    # #
+   #    #  #    ###    #   #   #  #    ###    #    #    # ###
+   #    #  #           #   #####  #           #    #    # #
+   #    #  ####        #   #   #  ####        #     ####  ####
+''')
