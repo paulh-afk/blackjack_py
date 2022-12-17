f9a424d5b67bc423a1bb868af72e064022b9bbb1
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-12-17 22:35:42 +0100

    get players names

---
+++ script.py
@@ -0,0 +1,5 @@
+player1_name = input('Player\'s name 1: ')
+player2_name = input('Player\'s name 2: ')
+
+print('Player 1 is {p1_name}, player 2 is {p2_name}'.format(
+    p1_name=player1_name, p2_name=player2_name))
