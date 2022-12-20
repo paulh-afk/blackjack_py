2b1be2add4a023adfee3e8c003969ce32e552169
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-12-20 18:53:14 +0100

    symbols characters

--- script.py
+++ script.py
@@ -1,4 +1,23 @@
-cards = ()
+cards = (
+    {
+        'symbol': 'heart',
+        'value': 1
+    },
+    {
+        'symbol': 'diamond',
+        'value': 1
+    },
+    {
+        'symbol': 'spade',
+        'value': 1
+    },
+    {
+        'symbol': 'club',
+        'value': 1
+    },
+)
+
+symbols = ('♥', '♦', '♠', '♣')
 
 game_cards = []
 dealer_cards = []
