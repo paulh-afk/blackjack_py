964ad4e9af696b81a44ec0422d82cad098766d48
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-12-20 18:50:19 +0100

    blackjack init

--- script.py
+++ script.py
@@ -1,28 +1,4 @@
-player1_name = input('Player\'s name 1: ')
-player2_name = input('Player\'s name 2: ')
+cards = ()
 
-print('Hello {p1_name} and {p2_name}, welcome to'.format(
-    p1_name=player1_name, p2_name=player2_name))
-
-print('''
-####### #  ####     #######  #    ####     #######  ####  ####
-   #       #           #    # #   #           #    #    # #
-   #    #  #    ###    #   #   #  #    ###    #    #    # ###
-   #    #  #           #   #####  #           #    #    # #
-   #    #  ####        #   #   #  ####        #     ####  ####
-''')
-
-print('The goal is to be the first player to get three in a row on a 3-by-3 grid')
-
-print('''
- -- -- --
-|  |  |  |
-|  |  |  |
- -- -- --
-|  |  |  |
-|  |  |  |
- -- -- --
-|  |  |  |
-|  |  |  |
- -- -- --
-''')
+game_cards = []
+dealer_cards = []
