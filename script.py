f6f3d9e6e36faf775bdcd4be02a560cd4a67a40e
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-12-21 01:15:34 +0100

    cards yaml file and file sha1 hash check

--- script.py
+++ script.py
@@ -1,221 +1,37 @@
-cards = (
-    # Heart (♥)
-    {
-        'symbol': 'heart',
-        'value': 1
-    },
-    {
-        'symbol': 'heart',
-        'value': 2
-    },
-    {
-        'symbol': 'heart',
-        'value': 3
-    },
-    {
-        'symbol': 'heart',
-        'value': 4
-    },
-    {
-        'symbol': 'heart',
-        'value': 5
-    },
-    {
-        'symbol': 'heart',
-        'value': 6
-    },
-    {
-        'symbol': 'heart',
-        'value': 7
-    },
-    {
-        'symbol': 'heart',
-        'value': 8
-    },
-    {
-        'symbol': 'heart',
-        'value': 9
-    },
-    {
-        'symbol': 'heart',
-        'value': 10
-    },
-    {
-        'symbol': 'heart',
-        'value': 'J'
-    },
-    {
-        'symbol': 'heart',
-        'value': 'Q'
-    },
-    {
-        'symbol': 'heart',
-        'value': 'K'
-    },
+import yaml
+import hashlib
 
-    # Diamond (♦)
-    {
-        'symbol': 'diamond',
-        'value': 1
-    },
-    {
-        'symbol': 'diamond',
-        'value': 2
-    },
-    {
-        'symbol': 'diamond',
-        'value': 3
-    },
-    {
-        'symbol': 'diamond',
-        'value': 4
-    },
-    {
-        'symbol': 'diamond',
-        'value': 5
-    },
-    {
-        'symbol': 'diamond',
-        'value': 6
-    },
-    {
-        'symbol': 'diamond',
-        'value': 7
-    },
-    {
-        'symbol': 'diamond',
-        'value': 8
-    },
-    {
-        'symbol': 'diamond',
-        'value': 9
-    },
-    {
-        'symbol': 'diamond',
-        'value': 10
-    },
-    {
-        'symbol': 'diamond',
-        'value': 'J'
-    },
-    {
-        'symbol': 'diamond',
-        'value': 'Q'
-    },
-    {
-        'symbol': 'diamond',
-        'value': 'K'
-    },
+# Read hash.txt
+try:
+    with open('hash.txt', 'r') as file:
+        check_sha1_hash = file.readline()
+except FileNotFoundError:
+    print('The file "hash.txt" does not exist')
+    exit()
 
-    # Spade (♠)
-    {
-        'symbol': 'spade',
-        'value': 1
-    },
-    {
-        'symbol': 'spade',
-        'value': 2
-    },
-    {
-        'symbol': 'spade',
-        'value': 3
-    },
-    {
-        'symbol': 'spade',
-        'value': 4
-    },
-    {
-        'symbol': 'spade',
-        'value': 5
-    },
-    {
-        'symbol': 'spade',
-        'value': 6
-    },
-    {
-        'symbol': 'spade',
-        'value': 7
-    },
-    {
-        'symbol': 'spade',
-        'value': 8
-    },
-    {
-        'symbol': 'spade',
-        'value': 9
-    },
-    {
-        'symbol': 'spade',
-        'value': 10
-    },
-    {
-        'symbol': 'spade',
-        'value': 'J'
-    },
-    {
-        'symbol': 'spade',
-        'value': 'Q'
-    },
-    {
-        'symbol': 'spade',
-        'value': 'K'
-    },
+# Read and check cards.yml hash
+try:
+    with open('cards.yml', 'rb') as file:
+        hasher = hashlib.sha1()
 
-    # Club (♣)
-    {
-        'symbol': 'club',
-        'value': 1
-    },
-    {
-        'symbol': 'club',
-        'value': 2
-    },
-    {
-        'symbol': 'club',
-        'value': 3
-    },
-    {
-        'symbol': 'club',
-        'value': 4
-    },
-    {
-        'symbol': 'club',
-        'value': 5
-    },
-    {
-        'symbol': 'club',
-        'value': 6
-    },
-    {
-        'symbol': 'club',
-        'value': 7
-    },
-    {
-        'symbol': 'club',
-        'value': 8
-    },
-    {
-        'symbol': 'club',
-        'value': 9
-    },
-    {
-        'symbol': 'club',
-        'value': 10
-    },
-    {
-        'symbol': 'club',
-        'value': "J"
-    },
-    {
-        'symbol': 'club',
-        'value': 'Q'
-    },
-    {
-        'symbol': 'club',
-        'value': 'K'
-    },
+        while True:
+            data = file.read()
+            if not data:
+                break
+            hasher.update(data)
 
-)
+        print(check_sha1_hash)
+        print(hasher.hexdigest())
+
+        if check_sha1_hash == hasher.hexdigest():
+            print('Valid file!')
+        else:
+            print('Invalid file')
+            exit()
+
+except FileNotFoundError:
+    print('The file "cards.yml" does not exist')
+    exit()
 
 card_symbols = ('♥', '♦', '♠', '♣')
 
