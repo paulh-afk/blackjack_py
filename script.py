383520af0c01fe62cf27e3b776196c0fd321956c
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-12-21 15:41:24 +0100

    shuffle_deck function

--- script.py
+++ script.py
@@ -1,5 +1,29 @@
 import yaml
 import hashlib
+import random
+
+
+def name_to_symbol(name: str) -> str:
+    return {
+        'heart': card_symbols[0],
+        'diamond': card_symbols[1],
+        'spade': card_symbols[2],
+        'club': card_symbols[3]
+    }.get(name, 'Invalid symbol!')
+
+
+def shuffle_deck(cards: list) -> list:
+    index_list = [*range(len(cards))]
+    shuffled_cards_deck = []
+
+    while len(index_list):
+        random_index = random.randrange(0, len(index_list))
+
+        shuffled_cards_deck.append(cards[index_list[random_index]])
+        index_list.pop(random_index)
+
+    return shuffled_cards_deck
+
 
 # Read "hash.txt"
 try:
@@ -31,23 +55,15 @@ except FileNotFoundError:
     print('The file "cards.yml" does not exist')
     exit()
 
-# Read and convert to tuple "cards.yml"
+# Read and convert "cards.yml" to tuple
 with open('cards.yml', 'r') as file:
     yaml_list = yaml.safe_load(file)
     cards = tuple(yaml_list)
 
     del yaml_list
 
+
 card_symbols = ('♥', '♦', '♠', '♣')
 
-game_cards = []
+deck_cards = shuffle_deck(cards)
 dealer_cards = []
-
-
-def name_to_symbol(name):
-    return {
-        'heart': card_symbols[0],
-        'diamond': card_symbols[1],
-        'spade': card_symbols[2],
-        'club': card_symbols[3]
-    }.get(name, 'Invalid symbol!')
