eccd6707c378a6e3e78dda62843611394da95245
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-02-05 16:59:49 +0100

    player __repr__

--- lib/player.py
+++ lib/player.py
@@ -1,13 +1,55 @@
-from .cards import distribute_cards
+from .cards import (
+    distribute_cards,
+    name_to_symbol,
+    is_honor_card,
+    honor_card_name_to_fullname,
+    honor_card_name_to_value,
+)
+from terminaltables import AsciiTable
 
 
 class Player:
-    name = ''
+    name = ""
     token_amount = 500
     cards = []
 
     def __init__(self, name: str, deck_cards: list) -> None:
+        self.name = name
         self.cards = distribute_cards(deck_cards)
 
-    def __str__(self) -> str:
-        return self.name + ' has ' + self.token_amount + ' tokens '
+    def __repr__(self) -> str:
+        table_data = [["Symbol", "Value"]]
+
+        for card in self.cards:
+            card_value = card["value"]
+            honor_value_name = None
+
+            if is_honor_card(card_value):
+                honor_value_name = honor_card_name_to_fullname(card_value)
+                card_value = honor_card_name_to_value(card_value)
+
+                if type(card_value) is tuple:
+                    card_value_list = [str(value) for value in card_value]
+                    card_value = "/".join(card_value_list)
+
+            row = [
+                name_to_symbol(card["symbol"])
+                + " ({symbol_name})".format(symbol_name=card["symbol"])
+            ]
+
+            if honor_value_name is None:
+                row.append(card_value)
+            else:
+                row.append(honor_value_name + " ({value})".format(value=card_value))
+
+            table_data.append(row)
+
+        table = AsciiTable(table_data, self.name)
+
+        return (
+            self.name
+            + " has "
+            + str(self.token_amount)
+            + " tokens and holds the cards:\n"
+            + table.table
+        )
