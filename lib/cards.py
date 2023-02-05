441d17ca1695f48c7522a5ddfe67c46d6acd9892
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-02-05 17:01:07 +0100

    handle honor_card

--- lib/cards.py
+++ lib/cards.py
@@ -1,17 +1,31 @@
-
 import random
 
-
-card_symbols = ('♥', '♦', '♠', '♣')
+card_symbols = ("♥", "♦", "♠", "♣")
+honor_cards = ("A", "J", "Q", "K")
 
 
 def name_to_symbol(name: str) -> str:
     return {
-        'heart': card_symbols[0],
-        'diamond': card_symbols[1],
-        'spade': card_symbols[2],
-        'club': card_symbols[3]
-    }.get(name, 'Invalid symbol!')
+        "heart": card_symbols[0],
+        "diamond": card_symbols[1],
+        "spade": card_symbols[2],
+        "club": card_symbols[3],
+    }.get(name, "Invalid symbol!")
+
+
+def is_honor_card(card: str) -> bool:
+    return bool(honor_cards.count(card))
+
+
+def honor_card_name_to_value(card: str) -> tuple | int:
+    if card == "A":
+        return (1, 11)
+    else:
+        return 10
+
+
+def honor_card_name_to_fullname(card: str) -> str:
+    return {"A": "Ace", "J": "Jack", "Q": "Queen", "K": "King"}.get(card)
 
 
 def shuffle_deck(cards: list) -> list:
