5f33d291fa6e91f5c2209cca26d310cbca9cc2e3
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-02-18 16:06:36 +0100

    Dealer request_player_tokens

--- lib/Dealer.py
+++ lib/Dealer.py
@@ -12,15 +12,36 @@ class Dealer(Player):
     def __repr__(self) -> str:
         return self.show_cards(True)
 
-    def request_player_choice(self, player: Player, deck_cards: list):
-        questions = [
+    def request_player_tokens(self, player: Player) -> int:
+        bet_choices = [25, 50, 100]
+
+        if bet_choices[0] > player.token_amount:
+            return 0
+
+        for i in range(1, len(bet_choices)):
+            if bet_choices[i] > player.token_amount:
+                bet_choices.pop(i)
+
+        question = [
             inquirer.List(
-                "bet", message="How much do you want to bet?", choices=[25, 50, 100]
+                "bet", message="How much do you want to bet?", choices=bet_choices
             ),
+        ]
+
+        bet_answer = inquirer.prompt(question)
+
+        if bet_answer is None:
+            print("Not a valid answer!")
+            exit()
+
+        return bet_answer["bet"]
+
+    def request_player_choice(self, player: Player, deck_cards: list) -> None:
+        question = [
             inquirer.Confirm("hit", message="Do you want to hit?", default=False),
         ]
 
-        answers = inquirer.prompt(questions)
+        answers = inquirer.prompt(question)
 
         if answers is None:
             print("Not a valid answer!")
@@ -29,7 +50,6 @@ class Dealer(Player):
         hit = answers["hit"]
 
         if player.total_cards_value() == BLACKJACK:
-            print("BLACKJACK!")
             hit = False
 
         while hit:
@@ -37,19 +57,9 @@ class Dealer(Player):
             print(player.show_cards(True))
 
             if player.total_cards_value() == BLACKJACK:
-                print("BLACKJACK!")
-                hit = False
+                break
 
             if player.total_cards_value() > BLACKJACK:
-                print(
-                    player.name
-                    + " the total value of the cards is above "
-                    + str(BLACKJACK)
-                    + ", you lose "
-                    + str(answers["bet"])
-                    + " tokens!"
-                )
-                player.token_amount -= answers["bet"]
                 break
 
             hit_answer = inquirer.prompt(
